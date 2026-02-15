caa5c7f47649b1938807b861af732adf2324a997
Rajesh Prajapati <rajeshprajapati@Rajeshs-MacBook-Air.local>
2026-02-15 17:59:34 +0530

    add new program Power Set Using Recursion

--- recursion/powerSetUsingRecursion.py
+++ recursion/powerSetUsingRecursion.py
@@ -1,3 +1,21 @@
+# Power Set Using Recursion
+
+"""
+Difficulty: EasyAccuracy: 47.78%Submissions: 46K+Points: 2
+You are given a string. You need to return the power-set (in any order) of the string.
+Note: The string s contains lowercase letter of alphabet.
+
+Examples:
+
+Input: s = a
+Output: ["","a"]
+Explanation: empty string and "a" are only sets.
+Input: s = abc
+Output: ["", "a", "ab", "abc", "ac", "b", "bc", "c"]
+Explanation: empty string, a, ab, abc, ac, b, bc, c are the sets.
+Constraints:
+1 ≤ s.length() ≤ 10
+"""
 def solve(ip,op,i,res):
     if i==len(ip):
         res.append(op)
